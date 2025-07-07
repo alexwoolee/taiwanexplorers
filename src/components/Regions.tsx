c270d0e8012222de78c8b27093f6928f3da9b7b8
Alex Lee <alexlee8937@gmail.com>
2025-07-07 14:12:47 -0700

    feat: added nav bar, organization of files, tested Google Maps API with react-google-maps library

---
+++ src/components/Regions.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { APIProvider, Map, type MapCameraChangedEvent } from '@vis.gl/react-google-maps';
+
+const Regions = () => {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  return (
+    <div className='flex flex-col w-full justify-center items-center'>
+      <h1>Map of Taipei</h1>
+      <div className='flex'>
+        <APIProvider apiKey={apiKey}>
+          <Map 
+            defaultZoom={13} 
+            center={{ lat: 25.0330, lng: 121.5654 }}
+            style={{ width: "500px", height: "500px"}}
+            onCameraChanged={ (ev: MapCameraChangedEvent) =>
+              console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+            }>  
+          </Map>
+        </APIProvider>
+      </div>
+    </div>
+  )
+};
+
+export default Regions;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Regions from "../components/Regions"
+
+const Home = () => {
+  return (
+    <>
+      <div id="bg-image"></div>
+      <Regions></Regions>
+    </>
+  )
+}
+
+export default Home
